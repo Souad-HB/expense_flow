1e2b65e1bfaebd77c3d0c78d94cf635197516ccf
Souad-HB <souadsalahh@gmail.com>
2025-01-22 15:00:48 -0500

    update account balance

--- server/src/controllers/account-controller.ts
+++ server/src/controllers/account-controller.ts
@@ -23,3 +23,31 @@ export const createAccount = async (req: Request, res: Response) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// update account balance
+// PUT /accounts/:id/balance
+export const updateAccountBalance = async (req: Request, res: Response) => {
+  const userId = req.params.id;
+  const { balance } = req.body;
+  try {
+    const user = await User.findByPk(userId);
+    if (!user) {
+      res.status(404).json("User not found");
+    } else {
+      const account = await Account.findOne({ where: { userId: userId } });
+      if (!account) {
+        res.status(404).json("No account associated with that user");
+      } else {
+        const newAccountBalance = await account.update(balance);
+        res
+          .status(200)
+          .json({
+            message: "balance updated successfully",
+            account: newAccountBalance,
+          });
+      }
+    }
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
