87f237baca2085b5dec57822db4abf29809af838
Souad-HB <souadsalahh@gmail.com>
2025-02-22 10:45:41 -0500

    added ./dist/**/* for tsconfig - added script for server build

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -49,7 +49,7 @@ export const Dashboard = () => {
     token: token,
   };
 
-  const { open, exit, ready } = usePlaidLink(config);
+  const { open, ready } = usePlaidLink(config);
   return (
     <button
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
