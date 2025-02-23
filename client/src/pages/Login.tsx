cf2f4f9c6d4278828f25aa070cea12a4a345c524
Souad-HB <souadsalahh@gmail.com>
2025-02-23 13:18:24 -0500

    updated login page ui

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { LoginFormData } from "../interfaces/LoginForm.js";
 import { login } from "../api/authAPI.js";
 import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
 
 export const Login = () => {
   const [formData, setFormData] = useState<LoginFormData>({
@@ -35,76 +36,88 @@ export const Login = () => {
   };
 
   return (
-    <div
-      className="relative min-h-screen bg-top bg-cover overflow-hidden top"
-      style={{ backgroundImage: "url('/assets/expenseflowimg1.png')" }}
-    >
-      <div className="max-w-full w-md m-auto flex flex-col shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-5 bg-gray-100">
-        <div className="w-full text-center text-2xl font-bold pb-10">
-          Expense Flow
+    <div className="w-full min-h-screen bg-gray-50 flex items-center justify-center p-4">
+      <div className="w-full bg-gray-100 rounded-2xl shadow-xl grid grid-cols-1 md:grid-cols-2 lg:w-[90%] lg:h-[90vh]">
+        {/* Left Column - Image */}
+        <div className="flex items-center justify-center">
+          <img
+            alt="landing img"
+            src="../public/assets/ex8.png"
+            className="w-full h-full object-cover rounded-l-2xl"
+          />
         </div>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="email"
-            >
-              Email:
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="email"
-              name="email"
-              type="email"
-              placeholder="Email Address"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
+        {/* Right Column - Login Form */}
+        <div className="flex flex-col items-center justify-center p-8">
+          <form onSubmit={handleSubmit} className="w-full max-w-md">
+            <div className="mb-6">
+              <label
+                htmlFor="email"
+                className="block text-zinc-800 text-xl font-bold mb-2"
+              >
+                Email:
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Email Address"
+                value={formData.email}
+                onChange={handleChange}
+                required
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
 
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
+            <div className="mb-6">
+              <label
+                htmlFor="password"
+                className="block text-zinc-800 text-xl font-bold mb-2"
+              >
+                Password:
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="**************"
+                value={formData.password}
+                onChange={handleChange}
+                required
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              />
+              {error && <p className="text-red-500 text-sm italic">{error}</p>}
+            </div>
+
+            <div className="flex items-center justify-center">
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{
+                  borderRadius: 50,
+                  backgroundColor: "#9a3c38",
+                  borderColor: "#9a3c38",
+                  color: "white",
+                  fontSize: "17px",
+                }}
+              >
+                Sign in
+              </Button>
+            </div>
+          </form>
+          <div className="pt-4 flex items-center justify-center text-sm">
+            <span className="text-gray-700">New to Expense Flow?</span>
+            <Button
+              onClick={() => navigate("/signup")}
+              className="underline pl-1 cursor-pointer"
+              sx={{ color: "#9a3c38" }}
             >
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border border-black-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              name="password"
-              placeholder="**************"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-            {error && <p className="text-red-500 text-xs italic">{error}.</p>}
+              Sign Up
+            </Button>
           </div>
-          <div className="flex items-center justify-center">
-            <button
-              className="bg-blue-600 hover:cursor-pointer text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-            >
-              Sign in
-            </button>
-          </div>
-        </form>
-        <div className="pt-2 flex flex-row justify-center text-sm">
-          New to Expense Flow?
-          <button
-            onClick={() => {
-              navigate("/signup");
-            }}
-            className="underline pl-1 cursor-pointer"
-          >
-            Sign up
-          </button>
+          <p className="text-center text-gray-500 text-xs pt-4">
+            &copy;2025 Souad Hassen-Bouzouita. All rights reserved.
+          </p>
         </div>
-        <p className="text-center text-gray-500 text-xs pt-4">
-          &copy;2025 Souad Hassen-Bouzouita. All rights reserved.
-        </p>
       </div>
     </div>
   );
